28355ec7e06a455ea9be8ce95eb5bb863a8d8608
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-10-14 17:12:46 +0000

    Updated

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-import express from "express";
-import { printly, colour } from "printly.js";
-
+const websuite = require("./config.json");
+const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(express.static('./views'));
+app.use(express.static('./public'));
+app.use(bodyParser.json())
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const rootDir = path.join(__dirname, "views");
+
+const makePath = function(file) {
+  return path.join(rootDir, file);
+}
+
+const getAccountFile = function(user) {
+  return path.join(path.join(__dirname, "accounts"), `${user}.json`);
+}
 
 app.get('/', async (req, res) => {
-  res.status(200)
-  .sendFile('/views/index.html')
+  res.render(makePath("index"), { title: websuite.title, logo: websuite.logo })
 });
 
-app.get('/auth/fariya', async (req, res) => {
-  res.status(200)
-  .sendFile('/views/fariya.html')
-});
+app.post('/auth/login', async (req, res) => {
+  const username = req.body.username || null;
+  const password = req.body.password || null;
+  if (username && password) {
+    let file = getAccountFile(username);
+    if (fs.exists(file)) {
+      file = readAccound(file);
+      if (password === file.password) {
+
+      } else {
+        // error mis match password
+      } else {
+        //error not found
+      res.redirect(`/?error=${websuite.error.notfound}`)
+      } else {
+        //error missing data
+        res.redirect(`/?error=${websuite.error.missing}`)
+      }
+    });
 
 app.listen(process.env.PORT, () => {
-  printly(colour.greenBright("Fariya is online..!"));
+  console.log("Fariya is online..!");
 });
