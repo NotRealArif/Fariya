7366281273e57e1ca31a5ac5c5865a0b50b869a2
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-10-14 17:26:06 +0000

    Updated and printly.js added

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+const { colour } = require("printly.js");
 
 app.use(express.static('./public'));
 app.use(bodyParser.json())
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const rootDir = path.join(__dirname, "views");
 
@@ -20,35 +21,39 @@ const getAccountFile = function(user) {
   return path.join(path.join(__dirname, "accounts"), `${user}.json`);
 }
 
-const readAccound = function (file){
-  
+const readAccound = function(file) {
+  return JSON.parse(fs.readFile(file, { encoding: 'utf-8' }));
 }
 
 app.get('/', async (req, res) => {
-  res.render(makePath("index"), { title: websuite.title, logo: websuite.logo })
+  const error = req.query.error || "";
+  res.render(makePath("index"), { title: websuite.title, logo: websuite.logo, error: error })
 });
 
 app.post('/auth/login', async (req, res) => {
   const username = req.body.username || null;
   const password = req.body.password || null;
+  console.log(req);
   if (username && password) {
     let file = getAccountFile(username);
     if (fs.existsSync(file)) {
       file = readAccound(file);
       if (password === file.password) {
-        res.send("hi")
+        res.status(200), json(file)
       } else {
         // error mis match password
         res.redirect(`/?error=${websuite.error.passnotmatch}`)
-      } else {
-        //error not found
-        res.redirect(`/?error=${websuite.error.notfound}`)
-      } else {
-        //error missing data
-        res.redirect(`/?error=${websuite.error.missing}`)
       }
-    });
+    } else {
+      //error not found
+      res.redirect(`/?error=${websuite.error.notfound}`)
+    }
+  } else {
+    //error missing data
+    res.redirect(`/?error=${websuite.error.missing}`)
+  }
+});
 
 app.listen(process.env.PORT, () => {
-  console.log("Fariya is online..!");
+  console.log(colour.greenBright("Fariya is online..!"));
 });
