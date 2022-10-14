0ef9acf69b72d5a458871876597b89e12f2b24b5
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-10-14 23:07:43 +0000

    Updated and added words to brain

--- hookChecker.js
+++ hookChecker.js
@@ -1,7 +1,26 @@
 
+const getHook = function(t){
+  if(t.includes("{")){
+    let _data = t.substr(t.indexOf('{') +1, t.length );
+      _data = _data.substr(0, _data.indexOf('}'))
+    return _data;
+  }else{
+    return null;
+  }
+}
+const setHookValue = function (txt, name, value){
+  return txt.replace(`{${name}}`, value);
+}
 
+function hooks(content, userdata){
+  const hook = getHook(content);
+  if(hook){
+    return setHookValue(content, hook, userdata[hook])
+  }
+  return content;
+}
 
 
 module.exports = {
-  checker: Ch
+  checker: hooks
 }
